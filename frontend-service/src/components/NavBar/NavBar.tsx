6b8fc4853d57baaa4d00673b7abc5ce6ff00635f
egarello <enzogarello5@gmail.com>
2025-07-13 13:56:53 -0300

    Agregado de estilos para la navbar

--- frontend-service/src/components/NavBar/NavBar.tsx
+++ frontend-service/src/components/NavBar/NavBar.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import './NavBar.css'
 const NavBar: React.FC = () => {
     return(
         <nav className="navbar">
-            <a href="/">Inicio</a>
-            <a href="/login">Acceder</a>
+            <div className="navbar-logo">
+                <Link to="/" className="navbar-link">Mi App</Link>
+            </div>
+            <div className="navbar-links">
+                <Link to="/login" className="navbar-link">Acceder</Link>
+            </div>
         </nav>
     );
 }
