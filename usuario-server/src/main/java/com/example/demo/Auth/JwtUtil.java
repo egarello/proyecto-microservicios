69b1db7ed3ae350aeabed856ce82db4c2bccb498
egarello <enzogarello5@gmail.com>
2025-07-15 23:07:36 -0300

    Solución del error. Ya inicia sesión, tuve que modificar versión del jsonwebtoken.

--- usuario-server/src/main/java/com/example/demo/Auth/JwtUtil.java
+++ usuario-server/src/main/java/com/example/demo/Auth/JwtUtil.java
@@ -1,27 +1,33 @@
 package com.example.demo.Auth;
 
-import java.util.Date;
-
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import java.security.Key;
+import java.util.Date;
 
 @Component
 public class JwtUtil {
+
     @Value("${jwt.secret}")
-    private String SECRET_KEY;
+    private String secretKey;
 
     @Value("${jwt.expirationMs}")
     private int jwtExpirationMs;
 
+    private Key getSigningKey() {
+        // Requiere al menos 32 bytes para HMAC-SHA256
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
     public String generateToken(String username) {
         return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs)) 
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
-                .compact();
+            .subject(username)
+            .issuedAt(new Date())
+            .expiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+            .signWith(getSigningKey()) // SIN pasar SignatureAlgorithm
+            .compact();
     }
 }
