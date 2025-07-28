db7407d7dc30f5ec774f55de49137379b7c8bdde
egarello <enzogarello5@gmail.com>
2025-07-27 21:27:18 -0300

    Agregado de la barra lateral con las opciones del perfil

--- frontend-service/src/components/NavBar/NavBar.tsx
+++ frontend-service/src/components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import './NavBar.css'
 
@@ -11,63 +11,77 @@ const NavBar: React.FC = () => {
     const isLoged = localStorage.getItem("token");
     const navigate = useNavigate();
     const [showProfileMenu, setShowProfileMenu] = useState(false);
+    // Efecto para bloquear/desbloquear el scroll
+    useEffect(() => {
+        if (showProfileMenu) {
+            document.body.classList.add('sidebar-open');
+        } else {
+            document.body.classList.remove('sidebar-open');
+        }
+
+        return () => {
+            document.body.classList.remove('sidebar-open');
+        };
+    }, [showProfileMenu]);
+
     const handleLogout = (opcion: string) => {
+        setShowProfileMenu(false);
         switch(opcion){
             case "logout":
                 localStorage.removeItem("token");
                 localStorage.removeItem("userData"); 
-                setShowProfileMenu(false);
                 navigate("/");
-                return
+                break;
             case 'editar':
                 navigate("/perfil/editar");
-                return
+                break;
         }
-
-    }
+    };
 
     return(
         <>
             <nav className="navbar">
                 <div className="navbar-logo">
-                    { isLoged ? (
-                        <Link to="/inicio" className="navbar-link">Mi App</Link>
-                    ):(
-                        <Link to="/" className="navbar-link">Mi App</Link>
-                    )}
-
+                    <Link to={isLoged ? "/inicio" : "/"} className="navbar-link">
+                        Mi App
+                    </Link>
                 </div>
                 <div className="navbar-links">
                     {isLoged ? (
                         <button 
                             className="navbar-link"
-                            onClick={() => {setShowProfileMenu(!showProfileMenu)}}
+                            onClick={() => setShowProfileMenu(!showProfileMenu)}
                         >
                             Mi Perfil
                         </button>
-                    ) : 
-                    (  
+                    ) : (
                         <Link to="/login" className="navbar-link">Acceder</Link>
                     )}
                 </div>
             </nav>
-            <div>
+            
+            <div className={`sidebar-profile-container ${showProfileMenu ? 'open' : 'close'}`}>
                 {showProfileMenu && (
+                    <>
+                    <div className="titulo-sidebar">
+                        <h4>Hola</h4>
+                        <button onClick={() => setShowProfileMenu(!showProfileMenu)}>X</button>
+                    </div>
                     <ul className="sidebar-profile">
                         {opciones.map(op => (
                             <li
                                 key={op.key}
-                                value={op.label}
+                                onClick={() => handleLogout(op.key)}
                             >
-                                <button onClick={()=>handleLogout(op.key)}>{op.label}</button>
+                                {op.label}
                             </li>
                         ))}
                     </ul>
+                    </>
                 )}
             </div>
         </>
-
     );
-}
+};
 
 export default NavBar;
